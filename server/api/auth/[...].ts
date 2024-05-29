b3b0cae5227548e95549d98eb1d4d0aae2ed5b34
OviOvocny <werint.pp@gmail.com>
2024-05-29 14:41:01 +0200

    add nonfake auth, see env example

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -5,6 +5,47 @@ import Credentials from "@auth/core/providers/credentials"
 
 const runtimeConfig = useRuntimeConfig()
 
+//
+
+const fakeAuth = Credentials({
+  name: "fake auth",
+  credentials: {
+    username: { label: "Username", type: "text", placeholder: "feta" },
+    password: { label: "Password", type: "password" },
+  },
+  async authorize(credentials, req) {
+    // Add logic here to look up the user from the credentials supplied
+    const user = { id: "1", name: "FETA", email: "feta@cheese.gr" }
+    if (user) {
+      // Any object returned will be saved in `user` property of the JWT
+      return user
+    } else {
+      return null
+    }
+  },
+})
+
+const credentialsAuth = Credentials({
+  name: "credentials",
+  credentials: {
+    username: { label: "Username", type: "text", placeholder: "feta" },
+    password: { label: "Password", type: "password" },
+  },
+  async authorize(credentials, req) {
+    // admin check
+    if (
+      credentials.username === process.env.ADMIN_USERNAME &&
+      credentials.password === process.env.ADMIN_PASSWORD
+    ) {
+      return { id: "admin", name: "Admin", email: "feta@cheese.gr" }
+    } else {
+      return null
+    }
+  },
+})
+
+//
+
 export const authOptions: AuthConfig = {
   basePath: "/api/auth",
   secret: runtimeConfig.authJs.secret,
@@ -12,23 +53,8 @@ export const authOptions: AuthConfig = {
     logo: "/icon.svg",
   },
   providers: [
-    Credentials({
-      name: "Fake Auth",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "feta" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "FETA", email: "feta@cheese.gr" }
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          return null
-        }
-      },
-    }),
+    // fakeAuth,
+    credentialsAuth,
   ],
 }
 
